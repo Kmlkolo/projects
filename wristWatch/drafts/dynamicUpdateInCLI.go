e4875b80b28e1cc8b9976c5c087e79c30b55fd18
kmowledge <116028665+kmowledge@users.noreply.github.com>
2024-03-05 21:11:57 +0100

    tried to attain dynamic updates view - with bufio scanner.

--- wristWatch/drafts/dynamicUpdateInCLI.go
+++ wristWatch/drafts/dynamicUpdateInCLI.go
@@ -1,42 +1,33 @@
+// GOAL: dynamic change of value in CLI. not skipping to newline triggered by sending the input.
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"time"
 )
-
-// GOAL: dynamic change of value in CLI. not skipping to newline triggered by sending the input.
 
 func main() {
 	var mode int
 
-	fmt.Println("Send \"4\" to switch mode.")
+	scanner := bufio.NewScanner(os.Stdin)
 
+	fmt.Println("Send \"4\" to switch mode.")
 	for {
 		var input string
+		scanner.Scan()
+		input = scanner.Text()
 
-		// Print the prompt
-		// fmt.Print("\r")
-
-		// Read user input
-		fmt.Scan(&input)
-
-		// Clear the current line
-		fmt.Print("\033[2K\r") //kod asci do czyszczenia linii
-
-		// Process the input
+		// fmt.Print("\033[2K\r") //kod asci do czyszczenia linii
 		switch input {
 		case "4":
 			mode++
 			fmt.Print("\r", mode)
-			time.Sleep(1 * time.Second)
-			fmt.Print("\033[2K\r")
+			// time.Sleep(1 * time.Second)
+			// fmt.Print("\033[2K\r")
 		default:
 			fmt.Print("\rCzwórkę wpisz.")
 		}
-
-		// Check for exit condition
 		if input == "exit" {
 			os.Exit(0)
 		}
