99bb7c2152ddff2cd670e7b5b99111df11ce39ff
kmowledge <116028665+kmowledge@users.noreply.github.com>
2024-03-07 15:55:33 +0100

    I try: dynamic cli update: \r, Fprint, \ASCII, bufio.Scanner

--- wristWatch/drafts/dynamicUpdateInCLI.go
+++ wristWatch/drafts/dynamicUpdateInCLI.go
@@ -2,34 +2,53 @@
 package main
 
 import (
-	"bufio"
+	_ "bufio"
 	"fmt"
 	"os"
+	_ "os"
+	"time"
 )
 
-func main() {
-	var mode int
+var z string = ""
 
-	scanner := bufio.NewScanner(os.Stdin)
-
-	fmt.Println("Send \"4\" to switch mode.")
-	for {
-		var input string
-		scanner.Scan()
-		input = scanner.Text()
-
-		// fmt.Print("\033[2K\r") //kod asci do czyszczenia linii
-		switch input {
-		case "4":
-			mode++
-			fmt.Print("\r", mode)
-			// time.Sleep(1 * time.Second)
-			// fmt.Print("\033[2K\r")
-		default:
-			fmt.Print("\rCzwórkę wpisz.")
-		}
-		if input == "exit" {
-			os.Exit(0)
+func Spinner(delay time.Duration) {
+	for z != "0" {
+		for _, r := range `-\|/` {
+			fmt.Printf("\r%c", r)
+			time.Sleep(delay)
+			fmt.Fprintf(os.Stdout, "\r \r")
 		}
 	}
 }
+
+func main() {
+	d, _ := time.ParseDuration("0.5s")
+	Spinner(d)
+}
+
+// func main() {
+// 	var mode int
+
+// 	scanner := bufio.NewScanner(os.Stdin)
+
+// 	fmt.Println("Send \"4\" to switch mode.")
+// 	for {
+// 		var input string
+// 		scanner.Scan()
+// 		input = scanner.Text()
+
+// 		// fmt.Print("\033[2K\r") //kod asci do czyszczenia linii
+// 		switch input {
+// 		case "4":
+// 			mode++
+// 			fmt.Print("\r", mode)
+// 			// time.Sleep(1 * time.Second)
+// 			// fmt.Print("\033[2K\r")
+// 		default:
+// 			fmt.Print("\rCzwórkę wpisz.")
+// 		}
+// 		if input == "exit" {
+// 			os.Exit(0)
+// 		}
+// 	}
+// }
